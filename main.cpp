39c2dd71e132f934554fc4db338df4a516e2e7d1
hahahanjihun <lego0811@naver.com>
2026-03-18 22:37:21 +0900

    힘들다

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
+// main.cpp
 #include <iostream>
 
+int add(int a, int b) {
+    return a + b;
+}
+
 int main() {
-    std::cout << "Hello VS Code!" << std::endl;
+    std::cout << add(1, 2) << std::endl;
     return 0;
 }
