d47191f2c652366f4259bd85bf71eecffad0da9a
hahahanjihun <lego0811@naver.com>
2026-05-01 22:52:04 +0900

    fix cmake test target and local test success

--- tests/my_test.cpp
+++ tests/my_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
-// 아주 간단한 테스트: 1과 1이 같은지 확인
-TEST(BasicTest, CheckOneOne) {
-    EXPECT_EQ(1, 1);
+int add(int, int);
+
+TEST(BasicTest, AddTest) {
+    EXPECT_EQ(add(1, 1), 2);
 }
