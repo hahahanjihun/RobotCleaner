e232efd4d12d82bf0c7c594286cc43e60fc0d156
Jae-ah <jaeahha@naver.com>
2026-05-02 13:09:43 +0900

    fix: endl

--- src/CleanerHandler.cpp
+++ src/CleanerHandler.cpp
@@ -6,20 +6,19 @@ CleanerHandler::CleanerHandler() = default;
 
 void CleanerHandler::activateCleaner() {
     powerLevel = PowerSetting::NORMAL;
-    std::cout << "[Cleaner] 청소기 흡입 ON" << std::endl;
+    std::cout << "[Cleaner] 청소기 흡입 ON\n";
 }
-
 void CleanerHandler::deactivateCleaner() {
     powerLevel = PowerSetting::OFF;
-    std::cout << "[Cleaner] 청소기 흡입 OFF" << std::endl;
+    std::cout << "[Cleaner] 청소기 흡입 OFF\n";
 }
 
 void CleanerHandler::boostPower() {
     powerLevel = PowerSetting::BOOST;
-    std::cout << "[Cleaner] 강력 흡입 모드" << std::endl;
+    std::cout << "[Cleaner] 강력 흡입 모드\n";
 }
 
 void CleanerHandler::normalizePower() {
     powerLevel = PowerSetting::NORMAL;
-    std::cout << "[Cleaner] 일반 모드 복구" << std::endl;
+    std::cout << "[Cleaner] 일반 모드 복구\n";
 }
--- src/MotorHandler.cpp
+++ src/MotorHandler.cpp
@@ -6,25 +6,25 @@ MotorHandler::MotorHandler() = default;
 
 void MotorHandler::moveForward() {
     status = DriveSetting::FORWARD;
-    std::cout << "[Motor] 전진" << std::endl;
+    std::cout << "[Motor] 전진\n";
 }
 
 void MotorHandler::moveBackward() {
     status = DriveSetting::BACKWARD;
-    std::cout << "[Motor] 후진" << std::endl;
+    std::cout << "[Motor] 후진\n";
 }
 
 void MotorHandler::turnLeft() {
     status = DriveSetting::LEFT;
-    std::cout << "[Motor] 좌회전" << std::endl;
+    std::cout << "[Motor] 좌회전\n";
 }
 
 void MotorHandler::turnRight() {
     status = DriveSetting::RIGHT;
-    std::cout << "[Motor] 우회전" << std::endl;
+    std::cout << "[Motor] 우회전\n" ;
 }
 
 void MotorHandler::stopMotor() {
     status = DriveSetting::STOPPED;
-    std::cout << "[Motor] 정지" << std::endl;
+    std::cout << "[Motor] 정지\n";
 }
